e0150e4b8b6d0f550a8125b5fee91d04b95b49b7
Pi-0r-Tau <147527008+Pi-0r-Tau@users.noreply.github.com>
2024-12-30 15:56:16 +0000

    background.js file version 0.0.1

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(() => {
+    console.log("Extension installed");
+});
+
+chrome.runtime.onMessage.addListener((request) => {
+    if (request.message === "clicked_browser_action") {
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            var activeTab = tabs;
+            chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
+            console.log(activeTab.url);
+        });
+    }
+});
